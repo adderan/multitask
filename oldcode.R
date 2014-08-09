15e1e06b517b8cbbc2b4063dec67e20208edb7be
Alden <aldenderan@yahoo.com>
2014-08-09 13:07:04 -0700

    -

--- oldcode.R
+++ oldcode.R
@@ -162,5 +162,248 @@ gray.analysis <- function(filename) {
 	}
 
 }
+#generate some test data. 
+generate.data <- function(f, g, n, epsilon) { #f is total # of features in data, g is number of prediction problems, g is number of true predictors,  n is number of feature vectors 	
+	x <- matrix(runif(f*n), f, n)
+	w <- norm(c(rep(1, times=g), rep(0, times=(f-g))))
+	y <- matrix(runif(g*n), g, n)
+	x <- normalize_matrix(x)
+	y <- normalize_matrix(y)
+
+	for(k in 1:(g/2)) {
+		for(i in 1:n) {
+			y[k, i] <- w%*%(x[, i]) + rnorm(1, mean = 0, sd = epsilon)
+		}
+
+
+	}
+	return(list(x, y))
+}
+
+#normalizes each feature vector in a matrix of feature vectors. Only useful for normalizing the test data. 
+normalize.matrix <- function(matr) {
+	n <- dim(matr)[[2]]
+	for(i in 1:n) {
+		matr[ ,i] <- norm(matr[ ,i])
+	}
+	return(matr)
+}
+		
+#normalizes a vector to one.		
+norm <- function(x) {
+	norm = 0;
+	for(i in 1:length(x)) {
+		norm <- norm + x[i]*x[i]
+	}
+	normed.vector <- x * (1/sqrt(norm))
+	return(normed.vector)
+}
+
+w.gradient.descent <- function(x, y, v, theta, restarts, step, iters) {
+	f <- dim(x)[[1]]
+	w.hat <- c(rep(0, f))
+	minobj <- 10000000
+	for(r in 1:restarts) {
+		new.w <- w.find.local.min.numeric(x, y, v, theta, step, iters)
+		new.w.obj <- f.obj1(t(x), y, new.w, v, theta)
+		if(new.w.obj < minobj) {
+			w.hat <- new.w
+			minobj <- new.w.obj
+		}
+	}
+	return(w.hat)
+}
+		
+w.find.local.min.numeric <- function(x, y, v, theta, step, iters) {
+	f <- dim(x)[[1]]
+	w <- c(runif(f, -1, 1))
+	gmin <- f.obj1(t(x), y, w, v, theta)
+	iter <- 0
+	while(iter < iters) {
+		wstep <- w - dgdw.numeric(x, y, w, v, theta, step)
+		gnew <- f.obj1(t(x), y, wstep, v, theta)
+		if(gnew > gmin) {
+			step <- step /2;
+		}
+		else {
+			w <- wstep;
+			gmin <- gnew;
+			iter <- iter + 1;
+		}
+	}
+	return(w)
+	
+}
+dgdw.numeric <- function(x, y, w, v, theta, step) {
+	f <- dim(x)[[1]]
+	dg <- c(rep(0, f))
+	for(k in 1:f) {
+		wstep <- w
+		wstep[k] <- wstep[k] + step
+		g0 <- f.obj1(t(x), y, w, v, theta)
+		g1 <- f.obj1(t(x), y, wstep, v, theta)
+		dg[k] <- (g1 - g0)/step
+	}
+	return(dg)
+}
+	
+ando.test.output <- function(data, h) {
+	#X <- data$X.list
+	#y <- data$y.list
+	m <- length(data$X.list)
+	p <- dim(data$X.list[[1]])[[1]]
+	print(m)
+	print(p)
+	u <- matrix(0, p, m)
+	mat <- array(runif(h*p), dim=c(h,p))
+	#theta <- qr.Q(qr(mat))
+	#print(t(theta) %*% theta)
+	#print(dim(theta))
+	lambda <- c(rep(1, times=m))
+	theta <- matrix(runif(h*p), h, p)
+	V.hat <- theta %*% u
+	W.hat <- w.min.matrix(data$X.list, data$y.list, u, theta)
+	u <- W.hat + t(theta) %*% V.hat
+	#theta <- theta_min(u, p, m, h, lambda)
+	#V.hat <- theta %*% u
+	#W.hat <- w_min_matrix(data$X.list, data$y.list, u, theta)
+	
+
+	return(list(W.hat = W.hat, V.hat = V.hat, Theta.hat = theta))
+}
+
+#calculate derivative of vT*theta*x for v minimization
+dhdVq <- function(x, q, theta) {
+	d <- 0
+	for(k in 1:length(x)) {
+		d <- d + x[k]*theta[q,k]
+	}
+	#cat("Dimension of dhdVq: ", length(d), "\n")
+	return(d)
+}
+
+
+#compute minimum v given w and theta. Used to minimize v for target problem.
+v.min <- function(x, y, w, theta) {
+	p <- dim(x)[[1]]
+	n <- dim(x)[[2]]
+	h <- dim(theta)[[1]]
+	
+	cat("p = ", p, "\n")
+	print(x)
+
+	coefficients <- matrix(0, h, h) 
+
+	for(q in 1:h) {
+		#coefficients.q <- c(length=h)
+		for(j in 1:h) {
+				coefficient.qj <- 0
+				for(i in 1:n) {
+					coefficient.qji <- 0
+					for(k in 1:p) {
+						coefficient.qji <- coefficient.qj + theta[j,k]*x[k,i]
+					}
+					coefficient.qji <- coefficient.qj * dhdVq(x[,i], q, theta)
+					coefficient.qj <- coefficient.qj + coefficient.qji
+				}
+				if(q==j) {
+					coefficient.qj <- coefficient.qj + 2
+				}
+				coefficients[q,j] <- coefficient.qj
+		}
+		#sum <- c(length=p)
+		#for(i in 1:n) {
+		#	sum <- sum + x[,i]*dhdVq(x[,i], q, theta)
+		#}
+		#coefficients[q,] <- theta %*% sum
+		#coefficients[q,q] <- coefficients[q,q] + 2
+	}
+	values <- c()
+	for(q in 1:h) {
+		value.q <- 0
+		for(i in 1:n) {
+			value.q <- (y[i] - t(w)%*%x[,i])*dhdVq(x[,i], q, theta)
+		}
+		values[q] <- value.q
+	}
+	v <- solve(coefficients, values)
+
+}
+v.gradient.descent <- function (x, y, w, theta, iters, restarts) {
+	n <- dim(x)[[2]]
+	p <- dim(x)[[1]]
+	h <- dim(theta)[[1]]
+	vmin <- c()
+	objmin <- 1000000
+	for(r in 1:restarts) {
+		v <- c(runif(h, -1, 1))
+		vobj <- f.obj1(t(x), y, w, v, theta)
+		stepsize <- 0.1
+		iter <- 0
+		while(iter < iters) {
+			vtest <- v - v.prime.numeric(x, y, w, v, theta, stepsize)
+			vtestobj <- f.obj1(t(x), y, w, vtest, theta)
+			if(vtestobj > vobj) stepsize <- stepsize/2
+			else {
+				v <- vtest
+				vobj <- f.obj1(t(x), y, w, v, theta)
+				iter <- iter + 1
+			}
+			#print(v.prime.numeric(x, y, w, v, theta, 0.000001))
+			print(vtestobj)
+		}
+		finalobj <- f.obj1(t(x), y, w, v, theta)
+		print(finalobj)
+		if(finalobj < objmin) {
+			vmin <- v
+			objmin <- finalobj
+		}
+	}
+	cat("Final ando training objective: ", objmin, "\n")
+	vmin
+}
+v.prime.numeric <- function(x, y, w, v, theta, step) {
+	source("ando.R")
+	h <- dim(theta)[[1]]
+	vprime <- c(rep(0, h))
+	for(j in 1:h) {
+		vstep <- v
+		vstep[j] <- vstep[j] + step
+		vstepobj <- f.obj1(t(x), y, w, vstep, theta)
+		vobj <- f.obj1(t(x), y, w, v, theta)
+		vprime[j] <- (vstepobj - vobj)/h
+	}
+	return(vprime)
+}
+
+v.prime <- function(x, y, w, v, theta) {
+	vprime <- c(length = length(v))
+	n <- dim(x)[[2]]
+	for(q in 1:length(v)) {
+		vprime.q <- 0
+		for(i in 1:n) {
+			vprime.q <- vprime.q + (t(w) %*% x[,i] + t(v) %*% theta %*% x[,i] - y[[i]])*dhdVq(x[,i],q,theta)
+		}
+		vprime[q] <- vprime.q
+	}
+	vprime
+}
+	
+optimize.labeled <- function(x, y, theta, iters) {
+	f <- dim(x)[[1]]
+	n <- dim(x)[[2]]
+	h <- dim(theta)[[1]]
+	print(h)
+	v <- c(runif(h, -1, 1))
+	w <- c(runif(f, -1, 1))
+	cat("Dimension of theta: ", dim(theta), "\n")
+	cat("Dimension of x: ", dim(x), "\n")
+	cat("Length of v:", h, "\n")
+	for(i in 1:iters) {
+		w <- w.min(x, y, v, theta)
+		v <- v.gradient.descent(x, y, w, theta, 100, 20)
+	}
+	list(W.hat = w, V.hat = v)
+}
 
 
