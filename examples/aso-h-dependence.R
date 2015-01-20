12cbe5afdce2089eaa4fe59f3af3f5faa379330b
Alden <aldenderan@yahoo.com>
2015-01-19 17:01:09 -0800

    -

--- examples/aso-h-dependence.R
+++ examples/aso-h-dependence.R
@@ -13,35 +13,39 @@ load(datafile)
 partitioned.data <- partition.data(X, Y)
 Y.labels <- partitioned.data$Y.labels
 Y.answers <- partitioned.data$Y.answers
-
-
-bad.data.removed <- remove.bad.data(partitioned.data$X.train, Y.labels, drug)
-drug.response <- bad.data.removed$drug.response
-X.train <- bad.data.removed$X
+cat("h\tDrug\tScore\n") 
+for(d in 7:length(args)) {
+	drug.target <- args[d]
+	drug <- unlist(strsplit(drug.target, split = "[.]"))[[1]]
+	auxiliary <- unlist(strsplit(drug.target, split = "[.]"))[[2]]
+	bad.data.removed <- remove.bad.data(partitioned.data$X.train, Y.labels, drug)
+	drug.response <- bad.data.removed$drug.response
+	X.train <- bad.data.removed$X
 
 	
-bad.test.data.removed <- remove.bad.data(partitioned.data$X.test, Y.answers, drug)
-X.test <- bad.test.data.removed$X
-drug.answers <- bad.test.data.removed$drug.response
+	bad.test.data.removed <- remove.bad.data(partitioned.data$X.test, Y.answers, drug)
+	X.test <- bad.test.data.removed$X
+	drug.answers <- bad.test.data.removed$drug.response
 
-Y.aux <- Xu[auxiliary,]
-Xu.reduced <- Xu
-Xu.reduced[auxiliary,] <- 0
+	Y.aux <- Xu[auxiliary,]
+	Xu.reduced <- Xu
+	Xu.reduced[auxiliary,] <- 0
 
-aso.X <- list()
-aso.Y <- list()
-aso.X[[drug]] <- X.train
-aso.Y[[drug]] <- drug.response
-aso.X[[auxiliary]] <- Xu.reduced
-aso.Y[[auxiliary]] <- Y.aux
+	aso.X <- list()
+	aso.Y <- list()
+	aso.X[[drug]] <- X.train
+	aso.Y[[drug]] <- drug.response
+	aso.X[[auxiliary]] <- Xu.reduced
+	aso.Y[[auxiliary]] <- Y.aux
 
 
-h0 <- 100
-hstep <- 20
-for(i in 1:5) {
-	aso.model <- aso.train(aso.X, aso.Y, primary = drug, h = h0, ANALYTIC = TRUE)
-	aso.predictions <- aso.predict(aso.model, X.test)
-	score <- cor(as.vector(aso.predictions), drug.answers, method = "spearman")
-	cat(h0, "\t", score, "\n")
-	h0 <- h0 + hstep
+	h0 <- 5
+	hstep <- 5
+	for(i in 1:2) {
+		aso.model <- aso.train(aso.X, aso.Y, primary = drug, h = h0, ANALYTIC = TRUE)
+		aso.predictions <- aso.predict(aso.model, X.test)
+		score <- cor(as.vector(aso.predictions), drug.answers, method = "spearman")
+		cat(h0, "\t", drug.target, "\t", score, "\n")
+		h0 <- h0 + hstep
+	}
 }
--- examples/graph-h.R
+++ examples/graph-h.R
@@ -2,14 +2,14 @@
 library(reshape2)
 library(ggplot2)
 args <- commandArgs()
+data <- read.table(args[6], header = TRUE)
 
-
-data <- melt(read.table(args[7], header = TRUE), id = "h")
-for(i in 8:length(args)) {
-	h.graph <- melt(read.table(args[i], header = TRUE), id = "h")
-	data <- rbind(data, h.graph)
-}
-colnames(data) <- c("h", "Drug", "Score")
+#data <- melt(read.table(args[7], header = TRUE), id = "h")
+#for(i in 8:length(args)) {
+#	h.graph <- melt(read.table(args[i], header = TRUE), id = "h")
+#	data <- rbind(data, h.graph)
+#}
+#colnames(data) <- c("h", "Drug", "Score")
 print(data)
 ggplot(data, aes(x = h, y = Score, colour = Drug)) + geom_line()
-ggsave(file = args[6])
+ggsave(file = args[7])
